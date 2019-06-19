1b32a257f6909b7283910f91875b94fca5420a86
juanpquiroga <juanpquiroga@gmail.com>
2019-06-19 07:33:03 -0500

    List comprehension, save, load

--- lib/cards.ex
+++ lib/cards.ex
@@ -18,7 +18,24 @@ defmodule Cards do
 
 
   def create_deck do
-    ["Ace","Two","Three"]
+    values = ["Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine", "Ten","Jack","Queen","King"]
+    suits = ["Spades","Clubs","Hearts","Diamonds"]
+
+    # Solucion 1
+    """
+    cards = for value <- values do
+      for suit <- suits do
+        "{value} of {suit}"
+      end
+    end
+
+    List.flatten(cards)
+    """
+
+    # Solucion 2
+    for value <- values, suit <- suits do
+        "#{value} of #{suit}"
+    end
   end
 
   @doc """
@@ -31,4 +48,28 @@ defmodule Cards do
   def contains?( deck, hand ) do
     Enum.member?(deck,hand)
   end
+
+  def deal( deck, hand_size ) do
+    Enum.split(deck,hand_size)
+  end
+
+  def save( deck, filename ) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename,binary)
+  end
+
+  def load( filename ) do
+    {result,data} = File.read(filename)
+    :erlang.binary_to_term data
+  end
+
+
+  # Generar cartas
+  # deck = Cards.create_deck
+
+  # Repartir
+  # deck_hand = Cards.deal(Cards.shuffle(deck),5)
+
+  # Repartir utilizando pattern matching
+  # { hand, rest_of_deck } = Cards.deal( Cards.shuffle(cards), 5 )
 end
