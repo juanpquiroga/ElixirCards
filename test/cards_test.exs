165fbeca004e4830a817f668c50e331afb292d0a
juanpquiroga <juanpquiroga@gmail.com>
2019-06-22 19:31:16 -0500

    Inclusion de pruebas especificas

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -10,4 +10,13 @@ defmodule CardsTest do
     assert Cards.hello() == "Hi there!"
   end
 
+  test "create_deck debe crear 52 cartas " do
+    deck_length = length( Cards.create_deck() )
+    assert deck_length == 52
+  end
+
+  test "verificar que barajar no retorna las mismas cartas" do
+    deck = Cards.create_deck()
+    refute deck == Cards.shuffle(deck)
+  end
 end
